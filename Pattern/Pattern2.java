062dac64caca8437d02eac90a732346e64af27d6
ShivtejPawar07 <shivtejpawar8@gmail.com>
2025-08-06 08:11:36 +0530

    s

--- Pattern/Pattern2.java
+++ Pattern/Pattern2.java
@@ -34,14 +34,14 @@ Inverted Half Pyramid
 			System.out.println();
 		}
 /*
-Hollow Rectangle
+Hollow Half Pyramid
 1
 12
 1 3
 1  4
 12345
 */
-		System.out.println("Hollow Rectangle");
+		System.out.println("Hollow Half Pyramid");
 		for(int i=1;i<=5;i++){
 			for(int j=1;j<=5;j++){
 				if(i==j ||j==1 || i==5)//i==j
@@ -72,21 +72,20 @@ Hollow Half Pyramid
 		}
 
 /*
-Full Pyramid or Center-Aligned Pyramid.
+Hollow Full Pyramid.
    1
   1 2
  1   3
-1     4
+1      4
 */
 		
-		System.out.println("Full Pyramid or Center-Aligned Pyramid.");
+		System.out.println("Hollow Full Pyramid.");
 		for(int i=1;i<=5;i++){
 			for(int j=1,a=i;j<=7;j++){
 				if(j==5-i){
-		
 				    System.out.print(1);
 				}
-				else if(j==3+i){
+				else if(i==5 ||j==3+i){
 					System.out.print(a++);
 				}
 				else
@@ -94,7 +93,83 @@ Full Pyramid or Center-Aligned Pyramid.
 			}
 			System.out.println();
 		}
-
+/*
+Full pyramid
+    1
+   232
+  34543
+ 4567654
+567898765
+*/		
+		System.out.println("Full pyramid");
+           int cnt=0;
+	       for(int i=1;i<=5;i++){
+			for(int j=1;j<=9;j++){
+				if(j>=6-i && j<=4+i){
+					if(j<=5)
+				    System.out.print(++cnt);
+				else
+					System.out.print(--cnt);
+				}
+              else{
+				  System.out.print(" ");
+			  }				
+			}
+			System.out.println();
+		}
+  /*
+  Hollow Full Pyramid.
+12345
+2  5
+3 5
+45
+5
+  */   
+		System.out.println("Hollow Full Pyramid.");
+		for(int i=1;i<=5;i++){
+			for(int j=1,a=i;j<=5;j++,a++){
+				if(i==1 || j==1 || j==6-i){
+				    System.out.print(a);
+				}
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+/*
+Hollow Full Pyramid.
+1
+12
+1 3
+1  4
+12345
+*/		
+		System.out.println("Hollow Full Pyramid.");
+		for(int i=1;i<=5;i++){
+			for(int j=1,a=1;j<=5;j++,a++){
+				if(i==5 || j==1 || i==j){
+				    System.out.print(a);
+				}
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+		
+		System.out.println("Hollow Full Pyramid");
+		for(int i=1;i<=6;i++){
+			
+			for(int j=1,a=1;j<=11;j++){
+				if(j==7-i || j==5+i|| (i==6 && j%2!=0)){
+					System.out.print(a);
+					a++;
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
 		
 	}	
 }
--- Pattern/Pattern3.java
+++ Pattern/Pattern3.java
@@ -273,11 +273,23 @@ Solid Diamond
 			}
 			System.out.println();
 		}	
-
+/*
+Hollow Diamond Inscribed in a Rectangle
+***** *****
+****   ****
+***     ***
+**       **
+*         *
+*         *
+**       **
+***     ***
+****   ****
+***** *****
+*/
         System.out.println("Hollow Diamond Inscribed in a Rectangle ");
-		for(int i=1;i<=5;i++){
-			for(int j=1;j<=9;j++){
-				if(j<=6-i || j>=4+i){
+		for(int i=1;i<=10;i++){
+			for(int j=1;j<=11;j++){
+				if((j<=6-i || j>=6+i && i<=5)||(j<=i-5 || j>=17-i && i>=6)){
 					System.out.print("*");
 				}
 				else{
@@ -285,9 +297,189 @@ Solid Diamond
 				}
 			}
 			System.out.println();
-		}			
+		}	
+/*
+Solid Rombus
+   ****
+  ****
+ ****
+****
+*/
+        System.out.println("Solid Rombus");
+		for(int i=1;i<=4;i++){
+			for(int j=1;j<=7;j++){
+				if(j>=5-i && j<=8-i){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+/*
+Hollow Rombus
+   ****
+  *  *
+ *  *
+****
+*/
+        System.out.println("Hollow Rombus");
+		for(int i=1;i<=4;i++){
+			for(int j=1;j<=7;j++){
+				if(j==5-i || j==8-i || i==4 && j<=4 || i==1 && j>=4){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+/*
+Diagonal and sides of rectangle
+*******
+**   **
+* * * *
+*  *  *
+* * * *
+**   **
+*******
+*/		
+        System.out.println("Diagonal and sides of rectangle");
+		for(int i=1;i<=7;i++){
+			for(int j=1;j<=7;j++){
+				if(i==1 || j==1 || i==7|| j==7|| j==8-i || i==j){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+/*
+Diagonal & Sides of a Rhombus/Diamond
+*   *   *
+ * * * *
+  *   *
+ * * * *
+*   *   *
+ * * * *
+  *   *
+ * * * *
+*   *   *
+*/	
+		System.out.println("Diagonal & Sides of a Rhombus/Diamond");
+		for(int i=1;i<=9;i++){
+			for(int j=1;j<=9;j++){
+				if( (j==6-i || j==4+i && i<=5 ) || (j==i-4 || j==14-i && i>5 ) ||(i==j)||j==10-i){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+/*
+Diagonal & Sides of a Rhombus/Diamond
+    *
+   ***
+  * * *
+ *  *  *
+*********
+ *  *  *
+  * * *
+   ***
+    *
+*/
+		System.out.println("Diagonal & Sides of a Rhombus/Diamond");
+		for(int i=1;i<=9;i++){
+			for(int j=1;j<=9;j++){
+				if( (j==6-i || j==4+i && i<=5 ) || (j==i-4 || j==14-i && i>5 ) ||i==5||j==5){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+	/*
+	Butterfly Pattern Printing
+*       *
+**     **
+***   ***
+**** ****
+*********
+**** ****
+***   ***
+**     **
+*       *
+	
+	*/
+		System.out.println("Butterfly Pattern Printing");
+		for(int i=1;i<=9;i++){
+			for(int j=1;j<=9;j++){
+				if( ((j<=i || j>=10-i) && i<=5 ) ||( (j<=10-i || j>=i) && i>=6 )){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+/*
+*       *
+Left Arrows
+    *
+     *
+      *
+       *
+*********
+       *
+      *
+     *
+    *
+*/
+		System.out.println("Left Arrows");
+		for(int i=1;i<=9;i++){
+			for(int j=1;j<=9;j++){
+				if(j==4+i || j==14-i || i==5){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+/*
+Right Arrows
+    *
+   *
+  *
+ *
+*********
+ *
+  *
+   *
+    *
+*/		
+		System.out.println("Right Arrows");
+		for(int i=1;i<=9;i++){
+			for(int j=1;j<=9;j++){
+				if(j==6-i ||j==i-4 || i==5){
+					System.out.print("*");
+				}
+				else{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		
 	}
-	
-	
-
 }
