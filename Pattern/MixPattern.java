760be37483b518844e1413c075476cda2169b9a0
ShivtejPawar07 <shivtejpawar8@gmail.com>
2025-08-09 20:34:34 +0530

    s

--- Pattern/MixPattern.java
+++ Pattern/MixPattern.java
@@ -23,6 +23,41 @@ Mixed Pattern
 			}
 			System.out.println();
 		}
+/*
+Mixed pyramid
+   1
+  2*2
+ 3*3*3
+4*4*4*4
+4*4*4*4
+ 3*3*3
+  2*2
+   1
+*/
+		System.out.println("Mixed pyramid ");
+           int cnt=64;		
+	       for(int i=1;i<=8;i++){
+			   boolean flag=true;
+			for(int j=1;j<=7;j++){
+				if(j>=5-i && j<=3+i && flag && i<=4){
+					System.out.print(i);
+				    flag=false;
+			    }
+                else if(j>=i-4 && j<=12-i && flag && i>4){
+					System.out.print(9-i);
+				    flag=false;
+				}
+                else if((j>=5- i && j<=3+i && !flag && i<=4) ||(j>=i-4 && j<=12-i && !flag && i>4)) {
+                    System.out.print("*");
+                 flag = true;
+                } 				
+                else{
+					System.out.print(" ");
+					flag=true;
+				}				
+			}
+			System.out.println();
+		}
 	
 	}
 }
