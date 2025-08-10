7a0ecba0e9dbe3a643634c05d85df7cf4dcc13da
ShivtejPawar07 <shivtejpawar8@gmail.com>
2025-08-10 16:28:13 +0530

    s

--- Pattern/MixPattern.java
+++ Pattern/MixPattern.java
@@ -129,6 +129,89 @@ Mixed Pattern
 			}
 			System.out.println();
 		}
-	
+/*
+Mixed Pattern
+   *
+  *1*
+ *121*
+*12321*
+ *121*
+  *1*
+   *
+*/	
+		System.out.println("Mixed Pattern");
+		for(int i=1;i<=7;i++){
+			int a=1;
+			for(int j=1;j<=7;j++){
+				if(j==5-i || j==3+i || j==i-3 || j==11-i){
+					System.out.print("*");
+				}
+				else if((j>=6-i && j<=2+i && i>=2 && i<=4)||(j>=i-2 && j<=10-i && i>=5 && i<=6) ){
+					if(j<4)					
+						System.out.print(a++);
+					else 
+						System.out.print(a--);
+				
+				}
+				else{
+					System.out.print(" ");
+				}
+				
+			}
+			System.out.println();
+		}
+/*
+Mixed Pattern
+*
+*1*
+*121*
+*12321*
+*121*
+*1*
+*
+*/	
+		System.out.println("Mixed Pattern");
+		for(int i=1;i<=7;i++){
+			int a=1;
+			for(int j=1;j<=7;j++){
+				if(j==5-i || j==3+i || j==i-3 || j==11-i){
+					System.out.print("*");
+				}
+				else if((j>=6-i && j<=2+i && i>=2 && i<=4)||(j>=i-2 && j<=10-i && i>=5 && i<=6) ){
+					if(j<4)					
+						System.out.print(a++);
+					else 
+						System.out.print(a--);
+				
+				}
+			}
+			System.out.println();
+		}
+/*
+What is Pascal’s Triangle?
+Pascal’s Triangle is a triangular arrangement of numbers where:
+The first and last number in each row is always 1.
+Every number in between is the sum of the two numbers above it from the previous row.
+Pascale Triangle
+1
+1 1
+1 2 1
+1 3 3 1
+1 4 6 4 1
+1 5 10 10 5 1
+1 6 15 20 15 6 1
+
+*/   
+     System.out.println("Pascale Triangle");
+     for (int i=0;i<7;i++) {
+            int num=1;
+            for (int j=0;j<7;j++) {			
+				if(j<=i){
+                System.out.print(num + " ");
+                num = num * (i - j) / (j + 1);
+				}	
+            }
+            System.out.println();
+        }
 	}
 }
